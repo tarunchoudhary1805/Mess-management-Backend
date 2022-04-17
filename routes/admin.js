97cf00d551b375cec0aa6ecf5e592509f836d213
Tarun <tarunchoudhary1805@gmail.com>
2022-04-17 09:46:20 +0530

    asd

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const router = express.Router();
 const requireLogin = require("../middlewares/requireLogin");
 const Expense = require("../models/expense");
+const Income = require("../models/income");
 
 router.get("/activateUserAccount/:id", async (req, res) => {
   const { id } = req.params;
@@ -36,6 +37,37 @@ router.post("/addExpense", requireLogin, async (req, res) => {
   try {
     req.user.password = undefined;
     const data = new Expense({
+      description,
+      amount,
+      modeOfPayment,
+      dateOfPayment,
+      updatedBy: req.user,
+    });
+    const response = await data.save();
+    console.log(response);
+    res.json({ status: "ok", message: "Success", response });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/getIncome", requireLogin, async (req, res) => {
+  try {
+    const data = await Income.find().populate("updatedBy", "fullName email");
+    console.log(data);
+    res.json({ data, status: "ok", message: "Success" });
+  } catch (error) {}
+});
+
+router.post("/addIncome", requireLogin, async (req, res) => {
+  console.log(req.body);
+  const { description, amount, modeOfPayment, dateOfPayment } = req.body;
+  if (!description || !amount || !modeOfPayment || !dateOfPayment) {
+    return res.json({ status: "error", error: "All Fields are required" });
+  }
+  try {
+    req.user.password = undefined;
+    const data = new Income({
       description,
       amount,
       modeOfPayment,
